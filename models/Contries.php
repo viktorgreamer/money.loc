6fcadd29d0a3ed8717b5b0b1ad99ae4bbcceeaf9
viktor <an.viktory@gmail.com>
2019-01-13 19:01:25 +0800

    add country model

---
+++ models/Contries.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contries".
+ *
+ * @property int $id
+ * @property string $name
+ */
+class Contries extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contries';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 256],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+}
